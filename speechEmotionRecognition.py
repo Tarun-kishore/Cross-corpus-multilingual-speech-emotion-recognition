1226675b15c69a6e803422c59cbbf1315bc18a88
naman1608 <1608naman@gmail.com>
2022-11-05 17:59:25 +0530

    added SVM

--- speechEmotionRecognition.py
+++ speechEmotionRecognition.py
@@ -1,3 +1,4 @@
+from __future__ import division, print_function
 import os,glob
 import numpy as np 
 import librosa
@@ -8,11 +9,12 @@ import torch
 from parselmouth.praat import call
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
-from pyAudioAnalysis import audioBasicIO
+#from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import ShortTermFeatures
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVC
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.feature_selection import SelectKBest,mutual_info_classif
@@ -85,10 +87,10 @@ def extractFeatures(fileName):
 
 def loadData(folderName, emotionsConditions):
     x,y = [] , []
-    num=150
+    num=250
     for folder in glob.glob(folderName):
         for file in glob.glob(folder+'/*.wav'):
-            # num=num-1
+            num=num-1
             if(num<=0):
                 break
             fileName = os.path.basename(file)
@@ -196,6 +198,166 @@ def cal_accuracy(y_test, y_pred):
     classification_report(y_test, y_pred))
 
 
+class SVM():
+    """
+        Simple implementation of a Support Vector Machine using the
+        Sequential Minimal Optimization (SMO) algorithm for training.
+    """
+
+    def __init__(self, max_iter=10000, kernel_type='linear', C=1.0, epsilon=0.001):
+        self.kernels = {
+            'linear': self.kernel_linear,
+            'quadratic': self.kernel_quadratic
+        }
+        self.max_iter = max_iter
+        self.kernel_type = kernel_type
+        self.C = C
+        self.epsilon = epsilon
+
+    def fit(self, X, y):
+        # Initialization
+        n, d = X.shape[0], X.shape[1]
+        alpha = np.zeros((n))
+        kernel = self.kernels[self.kernel_type]
+        count = 0
+        while True:
+            count += 1
+            alpha_prev = np.copy(alpha)
+            for j in range(0, n):
+                i = self.get_rnd_int(0, n-1, j)  # Get random int i~=j
+                x_i, x_j, y_i, y_j = X[i, :], X[j, :], y[i], y[j]
+                k_ij = kernel(x_i, x_i) + kernel(x_j, x_j) - \
+                    2 * kernel(x_i, x_j)
+                if k_ij == 0:
+                    continue
+                alpha_prime_j, alpha_prime_i = alpha[j], alpha[i]
+                (L, H) = self.compute_L_H(
+                    self.C, alpha_prime_j, alpha_prime_i, y_j, y_i)
+
+                # Compute model parameters
+                self.w = self.calc_w(alpha, y, X)
+                self.b = self.calc_b(X, y, self.w)
+
+                # Compute E_i, E_j
+                E_i = self.E(x_i, y_i, self.w, self.b)
+                E_j = self.E(x_j, y_j, self.w, self.b)
+
+                # Set new alpha values
+                alpha[j] = alpha_prime_j + float(y_j * (E_i - E_j))/k_ij
+                alpha[j] = max(alpha[j], L)
+                alpha[j] = min(alpha[j], H)
+
+                alpha[i] = alpha_prime_i + y_i*y_j * (alpha_prime_j - alpha[j])
+
+            # Check convergence
+            diff = np.linalg.norm(alpha - alpha_prev)
+            if diff < self.epsilon:
+                break
+
+            if count >= self.max_iter:
+                print("Iteration number exceeded the max of %d iterations" %
+                      (self.max_iter))
+                return
+        # Compute final model parameters
+        self.b = self.calc_b(X, y, self.w)
+        if self.kernel_type == 'linear':
+            self.w = self.calc_w(alpha, y, X)
+        # Get support vectors
+        alpha_idx = np.where(alpha > 0)[0]
+        support_vectors = X[alpha_idx, :]
+        return support_vectors, count
+
+    def predict(self, X):
+        return self.h(X, self.w, self.b)
+
+    def calc_b(self, X, y, w):
+        b_tmp = y - np.dot(w.T, X.T)
+        return np.mean(b_tmp)
+
+    def calc_w(self, alpha, y, X):
+        return np.dot(X.T, np.multiply(alpha, y))
+    # Prediction
+
+    def h(self, X, w, b):
+        return np.sign(np.dot(w.T, X.T) + b).astype(int)
+    # Prediction error
+
+    def E(self, x_k, y_k, w, b):
+        return self.h(x_k, w, b) - y_k
+
+    def compute_L_H(self, C, alpha_prime_j, alpha_prime_i, y_j, y_i):
+        if(y_i != y_j):
+            return (max(0, alpha_prime_j - alpha_prime_i), min(C, C - alpha_prime_i + alpha_prime_j))
+        else:
+            return (max(0, alpha_prime_i + alpha_prime_j - C), min(C, alpha_prime_i + alpha_prime_j))
+
+    def get_rnd_int(self, a, b, z):
+        i = z
+        cnt = 0
+        while i == z and cnt < 1000:
+            i = rnd.randint(a, b)
+            cnt = cnt+1
+        return i
+    # Define kernels
+
+    def kernel_linear(self, x1, x2):
+        return np.dot(x1, x2.T)
+
+    def kernel_quadratic(self, x1, x2):
+        return (np.dot(x1, x2.T) ** 2)
+
+
+def SMO(x_train, x_test, y_train, y_test):
+    #print(y_train)
+    # clf.fit(x_train, y_train)
+    # pred_lin = clf.score(x_test, y_test)
+    # print("Test Accuracy: ", pred_lin*100)
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.fit_transform(x_test)
+    svm_model_linear = SVC(kernel = 'poly', C = 0.1).fit(x_train, y_train) 
+    svm_predictions = svm_model_linear.predict(x_test) 
+    print("Results Using Support Vector Machine: ")
+    cal_accuracy(y_test, svm_predictions)
+    return svm_model_linear
+    # creating a confusion matrix 
+    #print(confusion_matrix(y_test, svm_predictions) )
+    # # print(y_train)
+    # y_train_int = []
+    # for y in y_train:
+    #     if y == 'angry':
+    #         y_train_int.append(1)
+    #     if y == 'happiness':
+    #         y_train_int.append(2)
+    #     if y == 'neutral':
+    #         y_train_int.append(3)
+    #     if y == 'sadness':
+    #         y_train_int.append(4)
+    # y_test_int = []
+    # for y in y_test:
+    #     if y == 'angry':
+    #         y_test_int.append(1)
+    #     if y == 'happiness':
+    #         y_test_int.append(2)
+    #     if y == 'neutral':
+    #         y_test_int.append(3)
+    #     if y == 'sadness':
+    #         y_test_int.append(4)
+    # d = dict([(y,x+1) for x,y in enumerate(sorted(set(y_train)))])
+    # d2 = dict([(y,x+1) for x,y in enumerate(sorted(set(y_test)))])
+    # model = SVM()
+    # support_vectors, iterations = model.fit(x_train, y_train_int)
+
+    # Support vector count
+    # sv_count = support_vectors.shape[0]
+
+    # Make prediction
+    # y_hat = model.predict(x_test)
+    # print("Results Using SMO")
+    # cal_accuracy(y_test_int, y_hat)
+
+
+
 def decisionTree(x_train,x_test,y_train,y_test):
     
     # selector= SelectKBest(mutual_info_classif, k=28)
@@ -278,7 +440,8 @@ def classify(x_train_pre,x_test_pre,y_train_pre,y_test_pre):
 
     dt_clf=decisionTree(x_train,x_test,y_train,y_test)
     rf_clf=random_forest(x_train,x_test,y_train,y_test)
-    ensemble(x_train,x_test,y_train,y_test,dt_clf,rf_clf,make_pipeline(StandardScaler(), SVC(gamma='auto')))
+    smo_clf=SMO(x_train,x_test,y_train,y_test)
+    ensemble(x_train,x_test,y_train,y_test,dt_clf,rf_clf,smo_clf)
 
 
 # X_train, X_test, y_train, y_test = train_test_split(urduX, urduY, test_size=0.33, random_state=42)
